f31673d94e8290578638f2f24dfaae8de0b936a4
Dorphia <alex.rybak333@gmail.com>
2024-04-26 16:37:28 -0400

    Set up the inquirer questions and start-up functions. Started on shapes.js and installed NPMs.

---
+++ index.js
@@ -0,0 +1,44 @@
+const inquirer = require('inquirer');
+const fs = require(`fs`);
+
+const questions = [
+    //3 characters, text color (keyword or hex), shape (circle triangle square 
+    //list, color for shape, svg generated...
+    {
+        type: "input", 
+        message: "Enter 3 letters for your SVG: ",
+        name: "text"
+    },
+    {
+        type: "input", 
+        message: "Input the color of your text (name or hex code): ",
+        name: "textcolor"
+    },
+    {
+        type: "rawlist", 
+        message: "Choose a shape: ",
+        name: "shape",
+        choices: ["Circle", "Square", "Triangle"]
+    },
+    {
+        type: "input", 
+        message: "Input the color of your shape (name or hex code): ", 
+        name: "shapecolor"
+    }
+];
+
+function writeToFile(fileName, data) {``
+    fs.writeFile("logo.svg", data,(err)=>{
+        if(err) throw err;
+    })
+}
+
+function start() {
+    inquirer.prompt(questions).then((answers)=>{
+        const string = generateMarkdown(answers);
+        writeToFile("logo.svg", string);
+    })
+        
+}
+
+start();
